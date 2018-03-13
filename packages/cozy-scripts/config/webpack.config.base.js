a603242cefda6689cf58bdc3d67e7dccfb678b74
CPatchane <code@patchane.com>
2018-03-13 23:54:13 +0100

    fix: remove now unused json-loader (already in webpack 4) :fire:

--- packages/cozy-scripts/config/webpack.config.base.js
+++ packages/cozy-scripts/config/webpack.config.base.js
@@ -27,10 +27,6 @@ module.exports = {
         }
       },
       {
-        test: /\.json$/,
-        loader: 'json-loader'
-      },
-      {
         test: /\.css$/,
         loader: extractor.extract({
           fallback: 'style-loader',
